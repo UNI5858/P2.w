6fa8cf195fdad7dc8a8bcd62f06dd99c238939b4
UNI5858 <114461153+UNI5858@users.noreply.github.com>
2022-09-27 13:31:45 +0900

    Add files via upload

---
+++ alpha/alpha.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+	int h;
+	int a;
+	int i = 1;
+	printf("몇층?\n");
+	scanf_s("%d", &h);
+	while (i <= h) {
+		a = 1;
+		while (a <= i) {
+			printf("o");
+			a++;
+		}
+		printf("\n");
+		i++;
+	}
+}
