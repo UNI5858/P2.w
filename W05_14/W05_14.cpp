52436bcb9a36df1d8c9975580eb03619a8c0327b
UNI5858 <114461153+UNI5858@users.noreply.github.com>
2022-09-27 14:22:29 +0900

    Add files via upload

---
+++ W05_14/W05_14.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main() {
+	int n;
+	int sum=0,e_sum=0,o_sum=0;
+	printf("n:");
+	scanf_s("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		sum += i;
+		if (i % 2 == 0) {
+			e_sum += i;
+		}
+		else {
+			o_sum += i;
+		}
+	}
+		printf("숫자의 합: %d, 짝수의 합: %d, 홀수의 합: %d", sum, e_sum, o_sum);
+
+}
