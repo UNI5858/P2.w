6d212e7cb4ccd185f7243384e130d2877bc23188
UNI5858 <114461153+UNI5858@users.noreply.github.com>
2022-10-04 15:17:04 +0900

    Add files via upload

---
+++ W06_13_2/W06_13_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+int main() {
+	int a[100];
+	int n;
+
+	printf("몇개?:");
+	scanf_s("%d", &n);
+	printf("숫자입력:");
+
+	int e_id_sum = 0;
+	int o_id_sum = 0;
+
+	int e_sum = 0;
+	int o_sum = 0;
+
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &a[i]);
+	}
+
+
+	for (int i = 0; i < n; i++) {
+
+		if (i % 2 == 0) {
+			e_id_sum += a[i];
+		}
+		else {
+			o_id_sum += a[i];
+		}
+		if (a[i] % 2 == 0) {
+			e_sum += a[i];
+		}
+		else {
+			o_sum += a[i];
+		}
+
+	}
+	printf("(1):%d (2):%d (3):%d (4):%d", e_id_sum, o_id_sum, e_sum, o_sum);
+}
